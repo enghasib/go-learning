45cefcc95dabb0c4dacd9f6474751c1c79bb5dd8
Al Hasib <hasib2653@gmail.com>
2025-07-28 01:44:05 +0600

    create first get route with habib vai 47 video

--- practice/habib/go-server/main.go
+++ practice/habib/go-server/main.go
@@ -1,21 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	body := r.Body
-	reqBody, err := io.ReadAll(body)
-	if err != nil {
-		fmt.Println(err)
+	fmt.Fprintln(w, "hello world")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "I'm Hasib, I'm a software engineer!")
+}
+
+type Product struct {
+	ID          int     `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	ImgUrl      string  `json:"img_url"`
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only accept GET request!", http.StatusBadRequest)
+		return
 	}
-	w.Write(reqBody)
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(productList)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	app := http.ser
+	mux := http.NewServeMux()
 
+	mux.HandleFunc("/hello", helloHandler)
+	mux.HandleFunc("/about", aboutHandler)
+	mux.HandleFunc("/products", productHandler)
+
+	fmt.Println("Server is running on port 3000")
+	err := http.ListenAndServe(":3000", mux)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+var productList = []Product{{ID: 1, Title: "Wireless Headphones", Description: "High-quality noise-cancelling headphones.", Price: 129.99, ImgUrl: "https://www.lovefoodhatewaste.com/sites/default/files/styles/twitter_card_image/public/2022-07/Citrus%20fruits.jpg.webp?itok=H1j9CCCS"},
+	{ID: 2, Title: "Smart Watch", Description: "Stylish smart watch with health tracking.", Price: 199.99, ImgUrl: "https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/05/apples-1296x728-header.jpg?w=1155&h=1528"},
+	{ID: 3, Title: "Running Shoes", Description: "Lightweight shoes for everyday running.", Price: 89.50, ImgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZbMOVB8a8wRQ6e-UKZggiu7-edRAN1GolPQ&s"},
+	{ID: 4, Title: "Wireless Headphones", Description: "High-quality noise-cancelling headphones.", Price: 129.99, ImgUrl: "https://www.lovefoodhatewaste.com/sites/default/files/styles/twitter_card_image/public/2022-07/Citrus%20fruits.jpg.webp?itok=H1j9CCCS"},
+	{ID: 5, Title: "Smart Watch", Description: "Stylish smart watch with health tracking.", Price: 199.99, ImgUrl: "https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/05/apples-1296x728-header.jpg?w=1155&h=1528"},
+	{ID: 6, Title: "Running Shoes", Description: "Lightweight shoes for everyday running.", Price: 89.50, ImgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZbMOVB8a8wRQ6e-UKZggiu7-edRAN1GolPQ&s"},
 }
